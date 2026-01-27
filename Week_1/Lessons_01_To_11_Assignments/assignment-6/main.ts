cfb4cb4be0f53c38a92f556aeb9738849274fddf
Mustafa Shehata <mustafashehata55@gmail.com>
2026-01-27 20:42:31 +0200

    Week_1: Add assignment-6 solution

---
+++ Week_1/Lessons_01_To_11_Assignments/assignment-6/main.ts
@@ -0,0 +1,9 @@
+// Assignment 06
+let nothing;
+let theName: string = "Elzero";
+function showMyDetails(a = "", b = "", c): string {
+  return `${a}${b}${c}`;
+}
+
+// // Replace ???? With The Available Variables As Argument To Get The Result
+console.log(showMyDetails(undefined, nothing, theName)); // Elzero
