6610d936aeec3b8617362ff6590f70b8da148c5d
Mustafa Shehata <mustafashehata55@gmail.com>
2026-01-29 11:53:28 +0200

    Week_2: Add assignment-2 solution

---
+++ Week_2-Lessons_12_To_21_Assignments/assignment-2/main.ts
@@ -0,0 +1,10 @@
+// Assignment 02
+// Write Your Code Here
+type mix = number | boolean;
+
+// Do Not Edit Here
+let myInfo: mix;
+myInfo = 1000; // No Problem Here
+myInfo = true; // No Problem Here
+
+console.log(myInfo);
